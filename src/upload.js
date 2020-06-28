bd406085367de3412d47c0774b05165f2965e077
Vittman <Vladimir@MacBook-Pro-Nikolaj.local>
2020-06-28 11:14:56 +0300

    amazon upload

--- src/upload.js
+++ src/upload.js
@@ -1,11 +1,25 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 
-const upload = multer({ dest: 'uploads/' });
+const s3 = new aws.S3({accessKeyId: process.env.AMAZON_KEY, secretAccessKey: process.env.AMAZON_SECRET});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'prismagram-upload',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  }),
+});
 export const uploadMiddleware = upload.single('file');
 
 export const uploadController = (req, res) => {
-  const {file} = req;
-  console.log(file);
-  res.end()
+  const {file:{location}} = req;
+  res.json({location})
 };
 
